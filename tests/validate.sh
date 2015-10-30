6778b62aec6e9a8dc2ab3df4608aa86595ad5d00
Waldo Jaquith <waldo@jaquith.org>
2015-10-30 16:05:54 -0400

    Add the validator script

---
+++ tests/validate.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Validate that the JSON is well-formed.
+find schemas -type f -exec jsonlint -q '{}' +
+find hunting -type f -exec jsonlint -q '{}' +
+find fishing -type f -exec jsonlint -q '{}' +
+
+# Validate all hunting and fishing JSON against their respective schemas.
+find hunting -type f -exec jsonlint -q -V schemas/1.0a/hunting.json '{}' +
+find fishing -type f -exec jsonlint -q -V schemas/1.0a/fishing.json '{}' +
