6cc5f97968d6cee26b1bc705e7b2e629aa47807f
Waldo Jaquith <waldo@jaquith.org>
2015-10-30 16:34:07 -0400

    Fix the command-line switches

--- tests/validate.sh
+++ tests/validate.sh
@@ -6,5 +6,5 @@ find hunting -type f -exec jsonlint -q '{}' +
 find fishing -type f -exec jsonlint -q '{}' +
 
 # Validate all hunting and fishing JSON against their respective schemas.
-find hunting -type f -exec jsonlint -q -V schemas/1.0a/hunting.json '{}' +
-find fishing -type f -exec jsonlint -q -V schemas/1.0a/fishing.json '{}' +
+find hunting -type f -exec jsonlint -q -V=schemas/1.0a/hunting.json '{}' +
+find fishing -type f -exec jsonlint -q -V=schemas/1.0a/fishing.json '{}' +
